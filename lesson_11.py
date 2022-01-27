6fb59f666b65ae5f3b74ace130ae61d808f7c291
Olga <Olga@meta.ua>
2022-01-27 20:34:56 +0200

    lesson_11.py

--- lesson_11.py
+++ lesson_11.py
@@ -18,37 +18,37 @@
 Сколько всего нужно закупить парт чтобы их хватило на всех учеников? Программа получает на вход три натуральных
  числа: количество учащихся в каждом из трех классов.Даны три целых числа. Определите, сколько среди них совпадающих. 
  '''
-first_class = int(input('Please eneter the first quantity of kids: '))
-second_class = int(input('Please eneter the second quantity of kids: '))
-third_class = int(input('Please eneter the third quantity of kids: '))
-tables = (first_class + second_class + third_class)/2
-if first_class == second_class:
-    if second_class == third_class:
-        print('Quantity of kids are the same in first_class, second_class and third_class:', first_class)
-    else:
-        print('Quantity of kids are the same in first_class and second_class:', first_class)
-elif first_class == third_class:
-    print('Quantity of kids are the same in first_class and third_class:', first_class)
-elif second_class == third_class:
-    print('Quantity of kids are the same in second_class and third_class:', second_class)
-print('We need :', tables, 'tables for 3 classes.')
+# first_class = int(input('Please eneter the first quantity of kids: '))
+# second_class = int(input('Please eneter the second quantity of kids: '))
+# third_class = int(input('Please eneter the third quantity of kids: '))
+# tables = (first_class + second_class + third_class)/2
+# if first_class == second_class:
+#     if second_class == third_class:
+#         print('Quantity of kids are the same in first_class, second_class and third_class:', first_class)
+#     else:
+#         print('Quantity of kids are the same in first_class and second_class:', first_class)
+# elif first_class == third_class:
+#     print('Quantity of kids are the same in first_class and third_class:', first_class)
+# elif second_class == third_class:
+#     print('Quantity of kids are the same in second_class and third_class:', second_class)
+# print('We need :', tables, 'tables for 3 classes.')
 
 
 
 
 
-'''
- Программа должна вывести одно из чисел: 3 (если все совпадают), 2 (если два совпадает) или 
- 0 (если все числа различны).'''
-a = int(input('Please eneter a number1: '))
-b = int(input('Please eneter a number2: '))
-c = int(input('Please eneter a number3: '))
-if a == b and b == c:
-    print(3)
-elif a == b or b == c or a == c:
-    print(2)
-else:
-    print(0)
+# '''
+#  Программа должна вывести одно из чисел: 3 (если все совпадают), 2 (если два совпадает) или
+#  0 (если все числа различны).'''
+# a = int(input('Please eneter a number1: '))
+# b = int(input('Please eneter a number2: '))
+# c = int(input('Please eneter a number3: '))
+# if a == b and b == c:
+#     print(3)
+# elif a == b or b == c or a == c:
+#     print(2)
+# else:
+#     print(0)
 
 
 '''
@@ -57,39 +57,39 @@ else:
  григорианским календарем, год является високосным, если его номер кратен 4, но не кратен 100, 
  а также если он кратен 400.
  '''
-a = input('Please enter a number:')
-
+# a = input('Please enter a number:')
+#
 
 
 #Камень, ножницы, бумага:
 #Rock Paper Scissors
-
-player1_score = 0
-player2_score = 0
-player1_choice = ''
-player2_choice = ''
-round = 3
-#start of game:
-for i in range(1, round + 1):
-    #Enter data
-    player1_choice = str(input("Enter your choice player 1 , please: [r], [s], [p] :"))
-    player2_choice = str(input("Enter your choice player 2 , please: [r], [s], [p] :"))
-    #Compare data
-    if player1_choice == 'r' :
-       if player1_choice == 's'
-           print('Player 1 win the round!')
-           player1_score = player1_score + 1
-       elif player2_choice == 'p'
-           print('Player 2 win the round!')
-           player2_score = player2_score + 1
-       else:
-          print('Draw round')
-if player1_score > player2_score:
-    print('Player 1 win the game!')
-elif player2_score > player1_score:
-    print('Player 1 win the game!')
-else:
-    print('Draw game!')
+#
+# player1_score = 0
+# player2_score = 0
+# player1_choice = ''
+# player2_choice = ''
+# round = 3
+# #start of game:
+# for i in range(1, round + 1):
+#     #Enter data
+#     player1_choice = str(input("Enter your choice player 1 , please: [r], [s], [p] :"))
+#     player2_choice = str(input("Enter your choice player 2 , please: [r], [s], [p] :"))
+#     #Compare data
+#     if player1_choice == 'r' :
+#        if player1_choice == 's'
+#            print('Player 1 win the round!')
+#            player1_score = player1_score + 1
+#        elif player2_choice == 'p'
+#            print('Player 2 win the round!')
+#            player2_score = player2_score + 1
+#        else:
+#           print('Draw round')
+# if player1_score > player2_score:
+#     print('Player 1 win the game!')
+# elif player2_score > player1_score:
+#     print('Player 1 win the game!')
+# else:
+#     print('Draw game!')
 
 
 
@@ -103,6 +103,146 @@ else:
 #         print('Player 2 win the round!')
 #         player2_score = player2_score + 1
 
+
+
+# if player1_choice == player2_choice:
+#     print('Draw round')
+# elif player1_choice == 'r':
+#     if player2_choice == 's':
+#         print('Player 1 win the round!')
+#         player1_score = player1_score + 1
+#     else:
+#         print('Player 2 win the round!')
+#         player2_score = player2_score + 1
+
+# game = True
+# global_player1_score = 0
+# global_player2_score = 0
+# while game:
+# #Variables
+#  player1_score = 0
+#  player2_score = 0
+#  player1_choice = ''
+#  player2_choice = ''
+#  rounds = 3
+# #Start of game
+#  for i in range(1,rounds+1):
+#      player1_choice = ''
+#      player2_choice = ''
+#     #Enter data
+#     while player1_choice != 'r' and player1_choice != 'p' and player1_choice != 's':
+#         player1_choice = str(input("Enter your choice player 1: [r],[p],[s] : "))#r
+#     while player2_choice != 'r' and player2_choice != 'p' and player2_choice != 's':
+#         player2_choice = str(input("Enter your choice player 2: [r],[p],[s] : "))#p
+#     #Compare data
+#     # if player1_choice == 'r':
+#     #     if player2_choice == 's':
+#     #         print('Player 1 win the round!')
+#     #         player1_score = player1_score + 1
+#     #     elif player2_choice == 'p':
+#     #         print('Player 2 win the round!')
+#     #         player2_score = player2_score + 1
+#     #     else:
+#     #         print('Draw round')
+#     if player1_choice == player2_choice:
+#         print('Draw round')
+#     elif player1_choice == 'r':
+#         if player2_choice == 's':
+#             print('Player 1 win the round!')
+#             player1_score = player1_score + 1
+#         else:
+#             print('Player 2 win the round!')
+#             player2_score = player2_score + 1
+#     elif player1_choice == 'p':
+#         if player2_choice == 'r':
+#             print('Player 1 win the round!')
+#             player1_score = player1_score + 1
+#         else:
+#             print('Player 2 win the round!')
+#             player2_score = player2_score + 1
+#     elif player1_choice == 's':
+#         if player2_choice == 'p':
+#             print('Player 1 win the round!')
+#             player1_score = player1_score + 1
+#         else:
+#             print('Player 2 win the round!')
+#             player2_score = player2_score + 1
+# #Compare score
+#  if player1_score > player2_score:
+#     print('Player 1 win the game!')
+#  elif player2_score > player1_score:
+#     print('Player 2 win the game!')
+#  else:
+#     print('Draw game!')
+# game = bool(input('If you want continue press any key: '))
+
+
+#import random
+game = True
+global_player1_score = 0
+global_player2_score = 0
+#game_type = 'pvp' #pve
+while game:
+    #Variables
+    name1 = input('Please, enter your name1: ')
+    name2 = input('Please, enter your name2: ')
+    player1_choice = ''
+    player2_choice = ''
+    player1_score = 0
+    player2_score = 0
+    rounds = 3
+    #Start of game
+    for i in range(1,rounds+1):
+        player1_choice = ''
+        player2_choice = ''
+        #Enter data
+        while player1_choice != 'r' and  player1_choice != 'p' and player1_choice != 's':
+            player1_choice = str(input(f"Enter your choice {name1} : [r],[p],[s] : "))
+        while player2_choice != 'r' and player2_choice != 'p' and player2_choice != 's':
+            player2_choice = str(input(f"Enter your choice {name2} : [r],[p],[s] : "))
+        #Compare data
+        # if player1_choice == 'r':
+        #     if player2_choice == 's':
+        #         print('Player 1 win the round!')
+        #         player1_score = player1_score + 1
+        #     elif player2_choice == 'p':
+        #         print('Player 2 win the round!')
+        #         player2_score = player2_score + 1
+        #     else:
+        #         print('Draw round')
+        if player1_choice == player2_choice:
+            print('Draw round')
+        elif player1_choice == 'r':
+            if player2_choice == 's':
+                print(f"{name1} win the round!")
+                player1_score = player1_score + 1
+            else:
+                print(f"{name2} win the round!")
+                player2_score = player2_score + 1
+        elif player1_choice == 'p':
+            if player2_choice == 'r':
+                print(f"{name1} win the round!")
+                player1_score = player1_score + 1
+            else:
+                print(f"{name2} win the round!")
+                player2_score = player2_score + 1
+        elif player1_choice == 's':
+            if player2_choice == 'p':
+                print(f"{name1} win the round!")
+                player1_score = player1_score + 1
+            else:
+                print(f"{name2} win the round!")
+                player2_score = player2_score + 1
+    #Compare score
+    if player1_score > player2_score:
+        print(name1, ' win the game!')
+        global_player1_score = global_player1_score + 1
+    elif player2_score > player1_score:
+        print(name2, ' win the game!')
+        global_player2_score = global_player2_score + 1
+    else:
+        print('Draw game!')
+    game = bool(input("If you want continue press any key : "))
 
 
 
