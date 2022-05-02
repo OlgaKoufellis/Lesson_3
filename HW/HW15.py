40f1b1b9bd5048f185ba88f5b85a29990f0338a5
Olga <Olga@meta.ua>
2022-05-02 15:59:55 +0300

    HW15.py

--- HW/HW15.py
+++ HW/HW15.py
@@ -3,17 +3,17 @@
 Напишите функцию, вычисляющую произведение элементов списка целых.
 Список передаётся в качестве параметра. Полученный результат возвращается из функции.
 '''
-def lis(list):
-    result = 1
-    for i in list:
-        result = result * i
-    print(result)
-    return result
-
-
-
-list = [1, 4, 9, 5]
-print(lis(list))
+# def lis(list):
+#     result = 1
+#     for i in list:
+#         result = result * i
+#     print(result)
+#     return result
+#
+#
+#
+# list = [1, 4, 9, 5]
+# print(lis(list))
 
 
 '''
@@ -22,12 +22,12 @@ print(lis(list))
  Список передаётся в качестве параметра. Полученный результат возвращается из функции.
 '''
 
-def lis(list):
-    return min(list)
-
-
-list = [1, 4, 9, 5]
-print(lis(list))
+# def lis(list):
+#     return min(list)
+#
+#
+# list = [1, 4, 9, 5]
+# print(lis(list))
 
 
 '''
@@ -66,38 +66,38 @@ print(simple_number(random_numbers))
   Из функции нужно вернуть количество удаленных элементов.
 '''
 ######## 1
-def delete_a_number():
-
-    list_of_numbers = [10, 4, 9, 5, 7, 1, 15, 89, 99, 99]
-    list_of_numbers.remove(delete)
-    list_of_numbers.remove(delete2)
-    print('updated list_of_numbers:', list_of_numbers)
-    return('Deleted:', delete, delete2)
-
-
-delete = 15
-delete2 = 99
-print(delete_a_number())
+# def delete_a_number():
+#
+#     list_of_numbers = [10, 4, 9, 5, 7, 1, 15, 89, 99, 99]
+#     list_of_numbers.remove(delete)
+#     list_of_numbers.remove(delete2)
+#     print('updated list_of_numbers:', list_of_numbers)
+#     return('Deleted:', delete, delete2)
+#
+#
+# delete = 15
+# delete2 = 99
+# print(delete_a_number())
 
 ######### 2
-def delete_a_number(delete):
-    list_of_numbers = [10, 4, 9, 5, 7, 1, 15, 89, 99, 99]
-    count = 0
-    for delete in list_delete:
-
-        while True:
-            if delete in list_of_numbers:
-                index = list_of_numbers.index(delete)
-                list_of_numbers.remove(list_of_numbers[index])
-                count += 1
-            else:
-                break
-    print('updated list_of_numbers:', list_of_numbers)
-    return('Deleted:', count)
-
-
-list_delete = [15, 99]
-print(delete_a_number(list_delete))
+# def delete_a_number(delete):
+#     list_of_numbers = [10, 4, 9, 5, 7, 1, 15, 89, 99, 99]
+#     count = 0
+#     for delete in list_delete:
+#
+#         while True:
+#             if delete in list_of_numbers:
+#                 index = list_of_numbers.index(delete)
+#                 list_of_numbers.remove(list_of_numbers[index])
+#                 count += 1
+#             else:
+#                 break
+#     print('updated list_of_numbers:', list_of_numbers)
+#     return('Deleted:', count)
+#
+#
+# list_delete = [15, 99]
+# print(delete_a_number(list_delete))
 
 
 '''
@@ -105,18 +105,18 @@ print(delete_a_number(list_delete))
 Напишите функцию, которая получает два списка в качестве параметра 
 и возвращает список, содержащий элементы обоих списков.
 '''
-def list_list(list_of_numbers_2):
-
-    new_list = list_of_numbers_1[:]
-    for num in list_of_numbers_2:
-        new_list.append(num)
-
-    return new_list
-
-list_of_numbers_1 = [10, 4, 9, 5, 7, 1, 15, 89,99]
-list_of_numbers_2 = [11, 7, 19, 4, 8, 11, 15, 57, 2]
-print(list_list(list_of_numbers_2))
-
+# def list_list(list_of_numbers_2):
+#
+#     new_list = list_of_numbers_1[:]
+#     for num in list_of_numbers_2:
+#         new_list.append(num)
+#
+#     return new_list
+#
+# list_of_numbers_1 = [10, 4, 9, 5, 7, 1, 15, 89,99]
+# list_of_numbers_2 = [11, 7, 19, 4, 8, 11, 15, 57, 2]
+# print(list_list(list_of_numbers_2))
+#
 
 
 
@@ -129,15 +129,15 @@ print(list_list(list_of_numbers_2))
 Функция возвращает новый список, содержащий полученные результаты.
 '''
 
-def count_power_of(list_of_numbers_1):
-    new_list = []
-
-    n = ''
-    for i in list_of_numbers_1:
-        n = i ** num
-        new_list.append(n)
-    return new_list
-
-list_of_numbers_1 = [10, 4, 9, 5, 7, 1, 15, 89,99]
-num = 2
-print(count_power_of(list_of_numbers_1))
+# def count_power_of(list_of_numbers_1):
+#     new_list = []
+#
+#     n = ''
+#     for i in list_of_numbers_1:
+#         n = i ** num
+#         new_list.append(n)
+#     return new_list
+#
+# list_of_numbers_1 = [10, 4, 9, 5, 7, 1, 15, 89,99]
+# num = 2
+# print(count_power_of(list_of_numbers_1))
